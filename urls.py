03dacf50eb28a94473011b25460f2e40fabd91d5
kodumurisainikhil <kodumurisainikhil@gmail.com>
2021-10-25 20:40:54 +0530

    sample commit

---
+++ urls.py
@@ -0,0 +1,13 @@
+
+from django.urls import path
+from .views import *
+
+
+
+urlpatterns = [
+
+    path("home/",home),
+    path('register/', userregistration),
+    path('login/', userlogin),
+    path('logout/', userlogout),
+]
